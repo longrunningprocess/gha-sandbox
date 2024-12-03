4e22b59315bc595dfb96c176d113b6b1cc345d26
billy clark <billy.clark@presciencelabs.org>
2024-12-03 15:48:57 -0500

    correcting the types

--- src/msaccess.py
+++ src/msaccess.py
@@ -1,5 +1,5 @@
 import pyodbc
 
+# https://github.com/mkleehammer/pyodbc/wiki/Connecting-to-Microsoft-Access
 access_jet_drivers = [x for x in pyodbc.drivers() if x.startswith('Microsoft Access Driver')]
-
-print("JET drivers found: " + access_jet_drivers)
+print("JET drivers found: " + ", ".join(access_jet_drivers))
