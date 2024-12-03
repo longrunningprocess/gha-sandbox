c6ff6199d06bc57be8994dd8b06dfe60548e943a
billy clark <billy.clark@presciencelabs.org>
2024-12-03 16:17:54 -0500

    can I run an installer like this?

--- src/msaccess.py
+++ src/msaccess.py
@@ -1,15 +1,14 @@
 import pyodbc
 
 # https://github.com/mkleehammer/pyodbc/wiki/Connecting-to-Microsoft-Access
-access_jet_drivers = [x for x in pyodbc.drivers() if x.startswith('Microsoft Access Driver')]
-print("JET drivers found: " + ", ".join(access_jet_drivers))
+access_drivers = [x for x in pyodbc.drivers() if x.startswith('Microsoft Access Driver')]
+print(access_drivers)
 
-
-conn_str = (
-    r'DRIVER={Microsoft Access Driver (*.mdb, *.accdb)};'
-    r'DBQ=D:\a\gha-sandbox\gha-sandbox\src\empty.mdb;'
-    )
-cnxn = pyodbc.connect(conn_str)
-crsr = cnxn.cursor()
-for table_info in crsr.tables(tableType='TABLE'):
-    print(table_info.table_name)
+# conn_str = (
+#     r'DRIVER={Microsoft Access Driver (*.mdb, *.accdb)};'
+#     r'DBQ=D:\a\gha-sandbox\gha-sandbox\src\empty.mdb;'
+#     )
+# cnxn = pyodbc.connect(conn_str)
+# crsr = cnxn.cursor()
+# for table_info in crsr.tables(tableType='TABLE'):
+#     print(table_info.table_name)
